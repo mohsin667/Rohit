5765b2fcd55cd5360755491d5007d9cb54fec5d7
Afreen Ansari <afreenansari@192.168.1.10>
2023-04-02 03:50:33 +0530

    working on feedbacks

--- src/components/ProfileSummary.js
+++ src/components/ProfileSummary.js
@@ -14,7 +14,7 @@ function ProfileSummary() {
           <address>Currently working as Product Designer at CarDekho Group | India.</address>
           <div className="bio">
             <p>An enthusiastic and professional Interactive Designer, who enjoys being part of a creative and productive team. I am interested in the future of interactive media, particularly digital technology and using industry-standard multimedia software packages, designing the interface, and testing the functionality of the end product. Having the urge to be pixel perfect and a strong understanding of a good UI UX is what keeps me motivated to perfect my work. Based in Delhi, India.</p>
-            <a href={Resume} target="_blank" download className="btn">Download CV</a>
+            <a href="/CV.pdf" target="_blank" download className="btn">Download CV</a>
           </div>
         </div>
       </div>
